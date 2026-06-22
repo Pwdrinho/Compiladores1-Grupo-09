3e1fd610668595279b01d5958034f0e5fa043574
Pedro Americo <dev.americo@gmail.com>
2026-06-22 08:29:58 -0300

    feat: alteração da estrutra de arvore sintática
    Co-authored-by: Enzo Menali <enzomenali@gmail.com>
    
    Co-authored-by: Antonio Lucas <antonio.lucasa06@gmail.com>

--- src/ast/ast.h
+++ src/ast/ast.h
@@ -1,11 +1,8 @@
 #ifndef AST_H
 #define AST_H
+#include "symtab.h" // pegamos o tipo Simbolo e TipoDado
 
-/*
- * Enum que representa os tipos de nós possíveis na árvore sintática.
- * Cada constante representa uma construção da linguagem:
- * programa, função, declaração, expressão, etc.
- */
+// Tipos de nó que vamos usar, como o no é generico a gente vai usar esse enum pra saber o que cada nó representa
 typedef enum {
     NO_PROGRAMA,
     NO_LISTA,
@@ -18,17 +15,12 @@ typedef enum {
     NO_ATRIBUICAO,
     NO_RETORNO,
 
-    /*
-     * NO_IF usa duas formas por limitação da AST binária:
-     * - if sem else: dir aponta direto para o bloco do if
-     * - if com else: dir aponta para NO_LISTA(bloco_if, bloco_else)
-     */
     NO_IF,
     NO_WHILE,
     NO_FOR,
 
-    NO_BINARIO,
-    NO_UNARIO,
+    NO_BINARIO, // para operações como +, -, *, /, etc.
+    NO_UNARIO, // para operações como -x, !x, etc.
 
     NO_IDENTIFICADOR,
     NO_NUMERO,
@@ -37,37 +29,28 @@ typedef enum {
     NO_VAZIO
 } TipoNo;
 
-/*
- * Estrutura básica de um nó da AST.
- *
- * tipo  -> indica qual construção esse nó representa.
- * valor -> guarda textos importantes, como nome de variável, número ou operador.
- * esq   -> filho esquerdo.
- * dir   -> filho direito.
- *
- * A árvore foi modelada como binária para simplificar a integração inicial.
- */
+// Estrutura do Nó da Arvore Sintática (AST)
 typedef struct NoAST {
-    TipoNo tipo;
-    char *valor;
-    struct NoAST *esq;
-    struct NoAST *dir;
+
+    TipoNo tipo; // Tipo Sintático (ex: NO_ATRIBUICAO, NO_BINARIO)
+    
+    char *valor; // Guarda textos literais, números ou operadores - usamos quando não utilizamos a tabela hash e para variáveis colocamos o nome intermediario
+    
+    TipoDado tipo_dado; // tipo de dado (ex: TIPO_DADO_INT, TIPO_DADO_FLOAT) 
+                          
+    Simbolo *simbolo; // ponteiro direto para a Tabela de Símbolos, não precisamos procurar a variável e aumenta a precisao e velocidade
+
+    struct NoAST *esq; // filho esquerdo
+    struct NoAST *dir; // filho direito
 } NoAST;
 
-/*
- * Cria dinamicamente um novo nó da AST.
- */
-NoAST* criar_no(TipoNo tipo, NoAST *esq, NoAST *dir, const char *valor);
+// recebe o tipo do nó, ponteiros para os filhos, valor literal e ponteiro para o símbolo (pode ser NULL se não for variável)
+NoAST* criar_no(TipoNo tipo, NoAST *esq, NoAST *dir, const char *valor, Simbolo *simbolo);
 
-/*
- * Imprime a árvore de forma recursiva.
- * Usada principalmente para debug.
- */
+// imprime a arvore para vermos como ela ficou, tomara que so mostre bons resultados
 void imprimir_ast(NoAST *raiz, int nivel);
 
-/*
- * Libera toda a memória alocada pela AST.
- */
+// libera a memoria alocada para a arvore, chamamos no final do main
 void liberar_ast(NoAST *raiz);
 
 #endif
